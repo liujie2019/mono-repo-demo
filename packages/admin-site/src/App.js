ba25b0276556feb4651e5bab971249ac2fba05e3
liujie <liujie09@kuaishou.com>
2021-01-05 10:52:21 +0800

    初始化

---
+++ packages/admin-site/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WaterForm from '@mono-repo-demo/common/components/WaterForm';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>管理员页面</h2>
+      <WaterForm />
+    </div>
+  );
+}
+
+export default App;
